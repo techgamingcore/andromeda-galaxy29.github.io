4ae02316e50af023bdce71eaf7387346a45e66e0
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-11-14 17:48:57 +0300

    Uploaded start page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Список</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <h1>Список моих работ</h1>
+    </div>
+    <div>
+        <ul>
+            <li><a href="aboutcats/cats.php">Сайт про котов</a></li>
+            <li><a href="diary/diary.html">Ежедневник</a></li>
+            <li><a href="landing/index.html">Лендинг</a></li>
+        </ul>
+    </div>
+</body>
+</html>
