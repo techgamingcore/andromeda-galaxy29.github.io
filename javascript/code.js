1f5d0ace4897928d869c27f1244dfcb12170ff94
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-11-28 17:05:16 +0300

    Update code.js

--- javascript/code.js
+++ javascript/code.js
@@ -1,1 +1,17 @@
-alert("Hello World!");
+season = prompt("Какое ваше любимое время года?");
+answer = " - ваш лучший выбор!"
+if(season == "зима"){
+    alert("Норвегия" + answer)
+}
+else if(season == "весна"){
+    alert("Placeholder Country" + answer)
+}
+else if(season == "лето"){
+    alert("Малдивы" + answer)
+}
+else if(season == "осень"){
+    alert("Канада" + answer)
+}
+else{
+    alert("Приезжайте в Россию! Для вас найдем любой климат.")
+}
