f6ddebca4ff1f967803131e566c8cc1822c362c0
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2023-12-12 17:09:14 +0300

    vk

--- m2/js arrays n functions/code.js
+++ m2/js arrays n functions/code.js
@@ -7,3 +7,43 @@ function printNumbers(){
     }
     document.getElementById("output").innerHTML=String(array);
 }
+
+function pushArrayRandom(array){
+    for(i = 0; i<array.length; i++){
+        array[i] = parseInt(Math.random()*100-50)
+    }
+    return array
+}
+
+function isEven(num){
+    return num%2==0
+}
+
+function showArrayToConsole(array){
+    console.log(array)
+}
+
+function showArrayToDocument(array){
+    document.getElementById("output").innerHTML=String(array);
+}
+
+function amountEvenInArray(array){
+    counter = 0
+    for(i = 0; i<array.length; i++){
+        if(isEven(array[i])){
+            counter++
+        }
+    }
+    return counter
+}
+
+function main(){
+    n = parseInt(document.getElementById("input").value);
+    array = Array.apply(null, Array(5))
+    pushArrayRandom(array)
+    if (amountEvenInArray(array) < 20){
+        showArrayToConsole(array)
+    }else{
+        showArrayToDocument(array)
+    }
+}
