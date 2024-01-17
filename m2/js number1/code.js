75c53868f33fa225e440f0b25c6ab7a92575bc7b
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-01-17 09:34:30 +0300

    Update code.js

--- m2/js number1/code.js
+++ m2/js number1/code.js
@@ -1,13 +1,15 @@
-a = prompt("a = ");
-b = prompt("b = ");
-c = prompt("c = ");
-
-D = Math.pow(b, 2) - (4 * a * c);
-if (D > 0) {
-    alert((-b + Math.sqrt(D)) / (2 * a));
-    alert((-b - Math.sqrt(D)) / (2 * a));
-} else if (D == 0) {
-    alert(-b / (2 * a));
-} else {
-    alert("Нет корней");
+function main(){
+    a = parseInt(document.getElementById("a").value);
+    b = parseInt(document.getElementById("b").value);
+    c = parseInt(document.getElementById("c").value);
+    
+    D = Math.pow(b, 2) - (4 * a * c);
+    if (D > 0) {
+        alert((-b + Math.sqrt(D)) / (2 * a));
+        alert((-b - Math.sqrt(D)) / (2 * a));
+    } else if (D == 0) {
+        alert(-b / (2 * a));
+    } else {
+        alert("Нет корней");
+    }
 }
