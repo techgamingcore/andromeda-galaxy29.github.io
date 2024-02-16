91b95faffce44f9f79e7a63c7064bfb5055677fb
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-02-16 18:56:59 +0300

    Update game

--- m2/flappy bird/game.js
+++ m2/flappy bird/game.js
@@ -12,12 +12,20 @@ pipe_top.src = "assets/pipe_top.png";
 var pipe_bottom = new Image();
 pipe_bottom.src = "assets/pipe_bottom.png";
 
-var gap = 32
+var gap = 64
+var y = 0
+var accel = 0
 
 function draw(){
     ctx.drawImage(bg, 0, 0);
     ctx.drawImage(pipe_top, 100, 0);
     ctx.drawImage(pipe_bottom, 100, 0 + pipe_top.height + gap);
+
+    accel += 1;
+    y += accel
+    ctx.drawImage(bird, 32, y)
+
+    requestAnimationFrame(draw);
 }
 
 pipe_bottom.onload = draw;
