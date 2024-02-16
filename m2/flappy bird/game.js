7b450ac1448b702dafb74ff7bd2a0a7ecf9d83f6
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-02-16 18:50:34 +0300

    Update game.js

--- m2/flappy bird/game.js
+++ m2/flappy bird/game.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 
 var bird = new Image();
 bird.src = "assets/bird.png";
@@ -12,8 +12,12 @@ pipe_top.src = "assets/pipe_top.png";
 var pipe_bottom = new Image();
 pipe_bottom.src = "assets/pipe_bottom.png";
 
+var gap = 32
+
 function draw(){
-    context.drawImage(bg, 0, 0);
+    ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(pipe_top, 100, 0);
+    ctx.drawImage(pipe_bottom, 100, 0 + pipe_top.height + gap);
 }
 
 pipe_bottom.onload = draw;
